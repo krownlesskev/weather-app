80b485a9168affef7270c1bcac36f6f4af961d6b
Kevin Sana <kevin.sana@outlook.com>
2022-04-25 16:10:58 -0500

    moved weather component out of app component, and added logic for finding the day of the week

--- src/App.js
+++ src/App.js
@@ -2,6 +2,44 @@
 import Axios from 'axios';
 import { useEffect } from 'react';
 
+const WeatherComponent = () => {
+  const currentDate = new Date();
+  const dayOfTheWeekNumber = currentDate.getDay();
+  let dayOfTheWeek = '';
+  switch (dayOfTheWeekNumber) {
+    case 0:
+      dayOfTheWeek = 'Sun';
+      break;
+    case 1:
+      dayOfTheWeek = 'Mon';
+      break;
+    case 1:
+      dayOfTheWeek = 'Tue';
+      break;
+    case 1:
+      dayOfTheWeek = 'Wed';
+      break;
+    case 1:
+      dayOfTheWeek = 'Thu';
+      break;
+    case 1:
+      dayOfTheWeek = 'Fri';
+      break;
+  }
+
+  return (
+    <div>
+      <h1 className='day-of-the-week'>{dayOfTheWeek}</h1>
+      <div className='weather-icon'></div>
+      <div>
+        <p className='min-temp'></p>
+        <p className='max-temp'></p>
+      </div>
+    </div>
+  );
+};
+
+
 function App() {
   const apiKey = '3f6820661329be846a6d89b4fa860d8f';
 
@@ -18,14 +56,6 @@ function App() {
       });
   }, []);
 
-  const WeatherComponent = () => {
-
-    return (
-      <div>
-        <h1>Weather Component</h1>
-      </div>
-    );
-  };
 
 
   return (
