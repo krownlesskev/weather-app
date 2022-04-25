0132c1a33e6bce1746f7e9adfb6b079d0bc251b4
Kevin Sana <kevin.sana@outlook.com>
2022-04-25 15:52:13 -0500

    instlalled axios, used a hook to make an API call on render, variables get defined from API call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// Using axios to handle get requests for APIs
+import Axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  const apiKey = '3f6820661329be846a6d89b4fa860d8f';
+
+  // This API call will run when the App component is rendered
+  // Once it is run, it will pull data from the API and assign it appropriate variables
+  useEffect(() => {
+    Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Winnipeg&appid=${apiKey}&units=metric`)
+      .then((res) => {
+        const response = res.data;
+        const currentMinTemp = res.data.main.temp_min;
+        const currentMaxTemp = res.data.main.temp_max;
+        const weatherIcon = res.data.weather.icon;
+        console.log(res.data);
+      });
+  }, []);
+
+  const WeatherComponent = () => {
+
+    return (
+      <div>
+        <h1>Weather Component</h1>
+      </div>
+    );
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <WeatherComponent />
     </div>
   );
 }
